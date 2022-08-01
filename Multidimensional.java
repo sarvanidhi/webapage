cd54ce51b9b32de27b768fd9a53b601322ddcc52
unknown <kishore.devops1989@gmail.com>
2022-08-01 11:17:19 -0700

    commit

---
+++ Multidimensional.java
@@ -0,0 +1,13 @@
+public class Multidimensional
+{
+public static void main(String args[])
+{
+int arr[][]={{2,7,9};{3,6,1};{2,7,2}};
+for(int i=0;i<3;i++)
+{
+for(int j=0;j<3,j++)
+ system.out.print(arr[i][j]+"");
+ system.out.print();
+}
+}
+} 
